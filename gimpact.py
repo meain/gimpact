6fd80905882bb6eafeb2a5cb8105295538e4847b
Abin Simon <abinsimon10@gmail.com>
2017-05-13 12:22:38 +0530

    fix up little stuff

--- gimpact.py
+++ gimpact.py
@@ -1,14 +1,12 @@
-# git log --format='%aN' | sort -u
+'''Simple module to get git stats'''
 
-# git log --shortstat --no-merges --branches --author= | grep -E "fil(e|es) changed" | awk '{files+=$1; inserted+=$4; deleted+=$6} END {print "files changed: ", files, "lines inserted: ", inserted, "lines deleted: ", deleted }'
-
-import sys
-import fire
 import subprocess
+import fire
 import progressbar
 
 
 class bcolors:
+    '''Colors for printing'''
     HEADER = '\033[95m'
     BLUE = '\033[94m'
     GREEN = '\033[92m'
@@ -18,11 +16,16 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-def get_stats(folder):
-    bar = progressbar.ProgressBar()
-    print 'Generating stats'
+def get_authors(folder):
     names = subprocess.check_output(["git log --format='%aN' | sort -u"], cwd=folder, shell=True)
     names = names.split('\n')[:-1]
+    return names
+
+def get_stats(folder):
+    '''Get stats for a for all users'''
+    bar = progressbar.ProgressBar()
+    print 'Generating stats'
+    names = get_authors(folder)
     stats = []
     for name in bar(names):
         command = 'git log --shortstat --no-merges --branches --author=\"' + name + '\" | grep -E "fil(e|es) changed" | awk \'{files+=$1; inserted+=$4; deleted+=$6} END {print files,inserted,deleted}\''
@@ -39,12 +42,14 @@ def get_stats(folder):
 
 def print_stas(folder):
     stats = get_stats(folder)
+    print("{:<45}{:<30}{:<30}{:<30}\n".format(bcolors.UNDERLINE+'Author name'+bcolors.ENDC, bcolors.UNDERLINE+'Files changed'+bcolors.ENDC,\
+            bcolors.UNDERLINE+'Insertions'+bcolors.ENDC, bcolors.UNDERLINE+'Deletions'+bcolors.ENDC))
     for stat in stats:
         name = bcolors.BOLD+stat['name']+bcolors.ENDC
         files_changed = bcolors.YELLOW+stat['files_changed']+bcolors.ENDC
         insertions = bcolors.GREEN+stat['insertions']+bcolors.ENDC
         deletions = bcolors.RED+stat['deletions']+bcolors.ENDC
-        print("{:<50}{:<20}{:<20}{:<20}\n".format(name, files_changed, insertions, deletions))
+        print("{:<50}{:<30}{:<30}{:<30}\n".format(name, files_changed, insertions, deletions))
 
 
 class GitStats(object):
